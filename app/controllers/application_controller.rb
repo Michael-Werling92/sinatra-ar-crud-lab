757727555a936844e5f0e7b395b70b0c1382542e
Michael <mfw6mw@virginia.edu>
2021-04-15 20:33:11 -0400

    commit tree d16dd4d0205782954a0cb46abcbfee0f0c780d43

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,48 @@
-
 require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+  set :views, 'app/views'
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    attrs = params
+    @article = Article.create(attrs)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    attrs = params[:article]
+    @article.update(attrs)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect to '/articles'
   end
 end
